5856e30fab54d6fd06e7b2435880fd59c6fb394d
Matheus Klauberg <mfklauberg@gmail.com>
2019-12-22 12:17:50 -0300

    feat: application

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+import { FileInput, ImageEditor } from './components';
+
 const App: React.FC = () => {
+  const [imageData, setImageData] = useState({});
+
+  // @ts-ignore
+  const { content, name } = imageData;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="l-app">
+      <div className="l-app__title">
+        Image Filter Application
+      </div>
+      <div className="l-app__content">
+        <FileInput onChange={setImageData} />
+        <ImageEditor name={name} src={content} />
+      </div>
     </div>
   );
 }
