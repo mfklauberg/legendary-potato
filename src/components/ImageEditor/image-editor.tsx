f28b13322546ab7ae99b6f1a27acbaa2e7fac040
Matheus Klauberg <mfklauberg@gmail.com>
2019-12-22 13:56:25 -0300

    feat: add missing filters to image-editor

--- src/components/ImageEditor/image-editor.tsx
+++ src/components/ImageEditor/image-editor.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, createRef, Component } from 'react';
+import React, { ChangeEvent, Component, ReactElement, createRef } from 'react';
 import { fabric } from 'fabric';
 
 import './image-editor.css';
@@ -11,31 +11,49 @@ interface Props {
 };
 
 interface State {
+  blurStrength: number;
+  sepiaStrength: number;
+  vintageStrength: number;
+  [key: string]: any;
 }
 
 class ImageEditor extends Component<Props, State> {
-  private canvasRef = createRef<HTMLCanvasElement>();
   private canvas!: fabric.Canvas;
+  private canvasRef = createRef<HTMLCanvasElement>();
+
   private objectScale = .25;
+
+  state = {
+    blurStrength: 0,
+    sepiaStrength: 1,
+    vintageStrength: 1
+  };
 
   componentDidMount(): void {
     const canvasElement = this.canvasRef.current as unknown as HTMLCanvasElement;
 
     fabric.textureSize = 4096;
 
-    this.canvas = new fabric.Canvas(canvasElement, {
-      interactive: false,
-    });
+    this.canvas = new fabric.Canvas(canvasElement);
   }
 
-  shouldComponentUpdate(lastProps: Props) {
+  shouldComponentUpdate(prevProps: Props, prevState: State): boolean {
     const { src } = this.props;
+    const { blurStrength, sepiaStrength, vintageStrength} = this.state;
 
-    return lastProps.src !== src;
+    if (blurStrength !== prevState.blurStrength) return true;
+    if (sepiaStrength !== prevState.sepiaStrength) return true;
+    if (vintageStrength !== prevState.vintageStrength) return true;
+
+    return prevProps.src !== src;
   }
 
-  componentDidUpdate(): void {
+  componentDidUpdate(prevProps: Props): void {
     const { src } = this.props;
+
+    if (prevProps.src === src) {
+      return;
+    }
 
     this.canvas.clear();
 
@@ -44,15 +62,16 @@ class ImageEditor extends Component<Props, State> {
         .set({ left: 0, top: 0 })
         .scale(this.objectScale);
 
-      this.lockObject(obj);
+      this.lockObjectMovement(obj);
 
       this.canvas.add(obj);
-      this.canvas.setWidth((image.width || 0) * this.objectScale);
+      this.canvas.setActiveObject(obj);
       this.canvas.setHeight((image.height || 0) * this.objectScale);
+      this.canvas.setWidth((image.width || 0) * this.objectScale);
     });
   }
 
-  lockObject = (obj: fabric.Object) => {
+  lockObjectMovement = (obj: fabric.Object): void => {
     obj.lockMovementX = true;
     obj.lockMovementY = true;
     obj.lockRotation = true;
@@ -64,7 +83,15 @@ class ImageEditor extends Component<Props, State> {
     obj.lockUniScaling = true;
   };
 
-  onDownloadClick = () => {
+  clearImageFilters = (image: fabric.Image): void => {
+    image.filters = [];
+  };
+
+  getImage = (): fabric.Image => {
+    return this.canvas.getActiveObject() as fabric.Image;
+  };
+
+  onDownloadClick = (): void => {
     const { name } = this.props;
 
     const [filename, fileformat] = name.split('.');
@@ -81,8 +108,80 @@ class ImageEditor extends Component<Props, State> {
     link.click();
   };
 
+  onChangeStrength = (property: string) => {
+    return (event: ChangeEvent<HTMLInputElement>): void => {
+      const value = Number(event.target.value);
+
+      this.setState({ [property]: value });
+    };
+  };
+
+  onBlurClick = (): void => {
+    const { blurStrength } = this.state;
+
+    const image = this.getImage();
+    this.clearImageFilters(image);
+
+    // @ts-ignore - missing Blur filter annotation
+    const blurFilter = new fabric.Image.filters.Blur({
+      blur: blurStrength
+    });
+
+    image.filters?.push(blurFilter);
+    image.applyFilters();
+
+    this.canvas.renderAll();
+  };
+
+  onSepiaClick = (): void => {
+    const { sepiaStrength: strength } = this.state;
+
+    const image = this.getImage();
+    this.clearImageFilters(image);
+
+    // There's a Sepia filter built in the Fabric library, but to control its strength, we have to
+    // use a ColorMatrix with the original matrix values multiplied by the strength varying 1 to n.
+    const filter = new fabric.Image.filters.ColorMatrix({
+      matrix: [
+        0.393 * strength, 0.769 * strength, 0.189 * strength, 0, 0,
+        0.349 * strength, 0.686 * strength, 0.168 * strength, 0, 0,
+        0.272 * strength, 0.534 * strength, 0.131 * strength, 0, 0,
+        0, 0, 0, 1, 0
+      ]
+    });
+
+    image.filters?.push(filter);
+    image.applyFilters();
+
+    this.canvas.renderAll();
+  };
+
+  onVintageClick = (): void => {
+    const { vintageStrength: strength } = this.state;
+    const image = this.getImage();
+
+    this.clearImageFilters(image);
+
+    // There's a Vintage filter built in the Fabric library, but to control its strength, we have to
+    // use a ColorMatrix with the original matrix values multiplied by the strength varying 1 to n.
+    const filter = new fabric.Image.filters.ColorMatrix({
+      matrix: [
+        0.62793 * strength, 0.32021 * strength , -0.03965 * strength, 0, 0.03784 * strength,
+        0.02578 * strength, 0.64411 * strength , 0.03259 * strength , 0, 0.02926 * strength,
+        0.04660 * strength, -0.08512 * strength, 0.52416 * strength , 0, 0.02023 * strength,
+        0, 0, 0, 1, 0
+      ],
+    });
+
+    image.filters?.push(filter);
+    image.applyFilters();
+
+    this.canvas.renderAll();
+  };
+
   renderControls(): null | ReactElement {
     const { src } = this.props;
+    const { blurStrength, sepiaStrength, vintageStrength } = this.state;
 
     if (!src) {
       return null;
@@ -93,6 +192,21 @@ class ImageEditor extends Component<Props, State> {
         <Button onClick={this.onDownloadClick} value="Download" />
 
         <span className="c-image-editor__label">Filters:</span>
+
+        <div className="c-image-editor__controls-item">
+          <input type="range" value={blurStrength} min="0" max="1" step="0.1" onChange={this.onChangeStrength('blurStrength')} />
+          <Button onClick={this.onBlurClick} value="Apply Blur" />
+        </div>
+
+        <div className="c-image-editor__controls-item">
+          <input type="range" value={sepiaStrength} min="1" max="2" step="0.1" onChange={this.onChangeStrength('sepiaStrength')} />
+          <Button onClick={this.onSepiaClick} value="Apply Sepia" />
+        </div>
+
+        <div className="c-image-editor__controls-item">
+          <input type="range" value={vintageStrength} min="1" max="2" step="0.1" onChange={this.onChangeStrength('vintageStrength')} />
+          <Button onClick={this.onVintageClick} value="Apply Vintage" />
+        </div>
       </div>
     );
   }
@@ -117,50 +231,3 @@ class ImageEditor extends Component<Props, State> {
 }
 
 export default ImageEditor;
-
-// function applyFilter(index, filter) {
-//   var obj = canvas.getActiveObject();
-//   obj.filters[index] = filter;
-//   var timeStart = +new Date();
-//   obj.applyFilters();
-//   var timeEnd = +new Date();
-//   var dimString = canvas.getActiveObject().width + ' x ' +
-//     canvas.getActiveObject().height;
-//   $('bench').innerHTML = dimString + 'px ' +
-//     parseFloat(timeEnd-timeStart) + 'ms';
-//   canvas.renderAll();
-// }
-
-// $('brownie').onclick = function() {
-//   applyFilter(4, this.checked && new f.Brownie());
-// };
-// $('vintage').onclick = function() {
-//   applyFilter(9, this.checked && new f.Vintage());
-// };
-// $('technicolor').onclick = function() {
-//   applyFilter(14, this.checked && new f.Technicolor());
-// };
-// $('polaroid').onclick = function() {
-//   applyFilter(15, this.checked && new f.Polaroid());
-// };
-// $('kodachrome').onclick = function() {
-//   applyFilter(18, this.checked && new f.Kodachrome());
-// };
-// $('blackwhite').onclick = function() {
-//   applyFilter(19, this.checked && new f.BlackWhite());
-// };
-// $('grayscale').onclick = function() {
-//   applyFilter(0, this.checked && new f.Grayscale());
-// };
-// $('average').onclick = function() {
-//   applyFilterValue(0, 'mode', 'average');
-// };
-// $('luminosity').onclick = function() {
-//   applyFilterValue(0, 'mode', 'luminosity');
-// };
-// $('lightness').onclick = function() {
-//   applyFilterValue(0, 'mode', 'lightness');
-// };
-// $('invert').onclick = function() {
-//   applyFilter(1, this.checked && new f.Invert());
-// };
---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+export { default as Button } from './button/button';
+export { default as FileInput } from './file-input/file-input';
+export { default as ImageEditor } from './ImageEditor/image-editor';
